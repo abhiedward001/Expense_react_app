aa8b0961be2324980300be7163d8935730113d43
abhiedward001 <iam001abhi@gmail.com>
2021-05-21 22:14:31 +0530

    updated dummy data

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Footer from './components/Footer';
 const dummydata = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
+    title: 'Laptop',
+    amount: 30000,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 40000, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
     title: 'Car Insurance',
-    amount: 294.67,
+    amount: 10000,
     date: new Date(2021, 2, 28),
   },
   {
     id: 'e4',
     title: 'New Desk (Wooden)',
-    amount: 450,
+    amount: 4500,
     date: new Date(2021, 5, 12),
   },
 ];
